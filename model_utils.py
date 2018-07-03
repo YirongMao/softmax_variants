df6bfd967275e60b7a7504087a1a7077c9b09bdd
Yirong <YirongMao@users.noreply.github.com>
2018-07-03 22:25:00 +0800

    Update model_utils.py

--- model_utils.py
+++ model_utils.py
@@ -39,6 +39,33 @@ class Net(nn.Module):
         ip2 = self.ip2(ip1)
         return ip1, ip2
 
+    
+class RingLoss(nn.Module):
+    def __init__(self, type='L2', loss_weight=1.0):
+        super(RingLoss, self).__init__()
+        self.radius = nn.Parameter(torch.Tensor(1))
+        self.radius.data.fill_(-1)
+        self.loss_weight = loss_weight
+        self.type = type
+
+    def forward(self, x):
+        x = x.pow(2).sum(dim=1).pow(0.5)
+        if self.radius.data[0] < 0: # Initialize the radius with the mean feature norm of first iteration
+            self.radius.data.fill_(x.mean().data[0])
+        if self.type == 'L1': # Smooth L1 Loss
+            loss1 = F.smooth_l1_loss(x, self.radius.expand_as(x)).mul_(self.loss_weight)
+            loss2 = F.smooth_l1_loss(self.radius.expand_as(x), x).mul_(self.loss_weight)
+            ringloss = loss1 + loss2
+        elif self.type == 'auto': # Divide the L2 Loss by the feature's own norm
+            diff = x.sub(self.radius.expand_as(x)) / (x.mean().detach().clamp(min=0.5))
+            diff_sq = torch.pow(torch.abs(diff), 2).mean()
+            ringloss = diff_sq.mul_(self.loss_weight)
+        else: # L2 Loss, if not specified
+            diff = x.sub(self.radius.expand_as(x))
+            diff_sq = torch.pow(torch.abs(diff), 2).mean()
+            ringloss = diff_sq.mul_(self.loss_weight)
+        return ringloss
+    
 
 class COCOLoss(nn.Module):
     """
